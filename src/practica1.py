2afa7d857838ef30510403e2b519e959771adfc7
julian-ferrari <julianferrarijuli@gmail.com>
2025-08-18 21:46:15 +0000

    Cambios Pytest

--- src/practica1.py
+++ src/practica1.py
@@ -1,15 +1,102 @@
+#! /usr/bin/python
+
+# 1ra Practica Laboratorio 
+# Teoria de Grafos
+# Consigna: Implementar los siguientes metodos
+
+import sys
+
+def mostrar_saludo():
+    print("Hola Mundo")
+
 def lee_grafo(grafo):
     """
     Recibe como argumento un grafo representado como una lista de tipo:
     Ejemplo Entrada: 
        ['3', 'A', 'B', 'C', 'A B', 'B C', 'C B']
-    
+
     donde el 1er elemento se corresponde a la cantidad de vértices,
     y por último las aristas existentes.
 
     Ejemplo retorno: 
         (['A','B','C'],[('A','B'),('B','C'),('C','B')])
     """
-    pass
+    n = int(grafo[0])
+    vertices = grafo[1:n+1]
+    aristas = [tuple(x.split()) for x in grafo[n+1:]]
+    return vertices, aristas
+
+
+def lee_grafo_archivo(file_path):
+    '''
+    Lee un grafo desde un archivo y devuelve su representacion como lista.
+    Ejemplo Entrada: 
+        3
+        A
+        B
+        C
+        A B
+        B C
+        C B
+    Ejemplo retorno: 
+        (['A','B','C'],[('A','B'),('B','C'),('C','B')])
+    '''
+    with open(file_path, 'r') as file:
+        lines = [line.strip() for line in file.readlines() if line.strip()]
+    
+    n = int(lines[0])
+    vertices = lines[1:n+1]
+    aristas = [tuple(line.split()) for line in lines[n+1:]]
+    return vertices, aristas
+
+def imprime_grafo_lista(grafo):
+    '''
+    Muestra por pantalla un grafo. El argumento esta en formato de lista.
+    Ejemplo de salida:
+        Vértices: A, B, C
+        Aristas: (A, B), (B, C), (C, B)
+    '''
+    vertices, aristas = grafo
+    print("Vértices:", ", ".join(vertices))
+    print("Aristas:", ", ".join(f"({a}, {b})" for a, b in aristas))
+
+#################### FIN EJERCICIO PRACTICA ####################
+def lee_entrada_1():
+    '''
+    Lee un grafo desde entrada estandar y devuelve su representacion como lista.
+    Ejemplo Entrada: 
+        3
+        A
+        B
+        C
+        A B
+        B C
+        C B
+    Ejemplo retorno: 
+        ['3', 'A', 'B', 'C', 'A B', 'B C', 'C B']
+    '''
+    data_input = []
+    
+    for line in sys.stdin:
+        if line == '\n':
+            break
+        else:
+            # Con strip() eliminamos los '\n' del final de c/line
+            data_input.append(line.strip())
+            
+    return data_input
+
+    
+def lee_entrada_2():
+    count = 0
+    try:
+        while True:
+            line = input()
+            count = count + 1
+            print ('Linea: [{0}]').format(line)
+    except EOFError:
+        pass
+    
+    print ('leidas {0} lineas').format(count)
 
 
--- src/practica2.py
+++ src/practica2.py
@@ -1,110 +1,161 @@
-def cuenta_grado(grafo):
-    """
-    Calcula el grado de cada vértice en un grafo no dirigido.
-
-    Entrada:
-        grafo: (vertices, aristas)
-
-
-    Ejemplo Entrada:
-        grafo = (['A','B','C'], [('A','B'), ('B','C'), ('C','B')])
-
-    Ejemplo Retorno:
-        {'A': 1, 'B': 3, 'C': 2}
-    """
-    pass
-
-def vertice_aislado(grafo):
-    """
-    Devuelve la lista de vértices aislados (sin aristas incidentes).
-
-    Ejemplo Entrada:
-        grafo = (['A','B','C','D','E'], [('A','B'), ('B','C'), ('C','B')])
-
-    Ejemplo Retorno:
-        ['D','E']
-    """
-    pass
-
-def componentes_conexas(grafo):
-    """
-    Obtiene las componentes conexas de un grafo no dirigido.
-
-    Ejemplo Entrada:
-        grafo = (['A','B','C','D','E'], [('A','B'), ('B','C'), ('C','B'), ('D','E')])
-
-    Ejemplo Retorno:
-        [['A','B','C'], ['D','E']]
-    """
-    pass
-
-def es_conexo(grafo):
-    """
-    Devuelve True si el grafo es conexo, False en caso contrario.
-
-    Ejemplo Entrada 1:
-        grafo = (['A','B','C'], [('A','B'), ('B','C')])
-    Ejemplo Retorno 1:
-        True
-
-    Ejemplo Entrada 2:
-        grafo = (['A','B','C','D'], [('A','B'), ('C','D')])
-    Ejemplo Retorno 2:
-        False
-    """
-    pass
+def cuenta_grado(grafo_lista):
+    '''
+    Muestra por pantalla los grados de los vertices de un grafo. 
+    El argumento esta en formato de lista.
     
-def es_completo(grafo):
-    """
-    Devuelve True si el grafo es completo (grafo no dirigido simple).
+    Ejemplo Entrada: 
+        (['A','B','C'],[('A','B'),('B','C'),('C','B')])
+    Ejemplo retorno: 
+        {'A': 1, 'B': 3, 'C': 2}
+    '''
 
-    Ejemplo Entrada 1:
-        grafo = (['A','B','C'], [('A','B'), ('A','C'), ('B','C')])
-    Ejemplo Retorno 1:
-        True
+    '''
+    vertices, aristas = grafo_lista
+    grados = {v: 0 for v in vertices}
 
-    Ejemplo Entrada 2:
-        grafo = (['A','B','C'], [('A','B'), ('B','C')])
-    Ejemplo Retorno 2:
-        False
-    """
-    pass
+    for origen, destino in aristas:
+        grados[origen] += 1
+        grados[destino] += 1
+
+    return grados
+    '''
+
+    vertices, aristas = grafo_lista
+    grados = {}
+
+    for v in vertices:
+        grados[v] = 0
+    for a, b in aristas:
+        grados[a] += 1
+        grados[b] += 1
+
+    return grados
+
+
+def vertice_aislado(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, obtiene una lista de los vértices aislado.
+    Ejemplo Entrada: 
+        (['A','B','C','D','E'],[('A','B'),('B','C'),('C','B')])
+    Ejemplo formato salida: 
+        ['D','E']
+    '''
+    grados = cuenta_grado(grafo_lista)
+    aislados = []
+
+    for v in grados:
+        if (grados[v] == 0):
+            aislados.append(v)
+    
+    return aislados
+
+def componentes_conexas(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, obtiene sus componentes conexas.
+    Ejemplo Entrada: 
+        (['A','B','C','D','E'],[('A','B'),('B','C'),('C','B'),('D','E')])
+    Ejemplo formato salida: 
+        [['A, 'B','C'], ['D','E']]
+    '''
+    nodos, aristas = grafo_lista
+
+    grafo = {}
+    for nodo in nodos:
+        grafo[nodo] = []
+
+    for a, b in aristas:
+        grafo[a].append(b)
+        grafo[b].append(a)
+
+    visitados = []
+    componentes = []
+
+    for nodo in nodos:
+        if nodo not in visitados:
+            componente = []
+            cola = [nodo]
+            while len(cola) > 0:
+                actual = cola.pop(0)
+                if actual not in visitados:
+                    visitados.append(actual)
+                    componente.append(actual)
+                    for vecino in grafo[actual]:
+                        if vecino not in visitados and vecino not in cola:
+                            cola.append(vecino)
+            componentes.append(componente)
+
+    return componentes
+
+def es_conexo(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, y utilizando la función "componentes_conexas"
+    devuelve True/False si el grafo es o no conexo.
+    '''
+    componentes = componentes_conexas(grafo_lista)
+    return len(componentes) == 1
+
+def es_completo(grafo_lista):
+    '''
+    Dado un grafo en representacion de lista, devuelve True/False si el grafo es o no completo.
+    Ejemplo Entrada:
+    	['3', 'A', 'B', 'C', 'A B', 'B A', 'A C', 'C A', 'B C', 'C B']
+    Ejemplo formato salida:
+    	True
+    '''
+    vertices, aristas = grafo_lista
+    n = len(vertices)
+    e = len(aristas)
+
+    max_aristas = ((n * (n+1)) / 2)
+
+    return e == max_aristas
     	
 def aristas_de(grafo, vertice):
-    """
-    Devuelve todas las aristas incidentes a un vértice dado.
-
+    '''
+    Dado un grafo en representacion de lista, devuelva todas las aristas salientes desde un vértice dado
     Ejemplo Entrada:
-        grafo = (['A','B','C'], [('A','B'), ('A','C'), ('B','C')])
-        vertice = 'A'
-
-    Ejemplo Retorno:
-        [('A','B'), ('A','C')]
-    """
-    pass
-
+    	grafo = ['3', 'A', 'B', 'C', 'A B', 'A C', 'B C']
+	aristas_de(grafo, 'A')
+    Ejemplo formato salida:
+    	[('A', 'B'), ('A', 'C')]
+    '''
+    vertices, aristas = grafo
+    
+    return [arista for arista in aristas if arista[0] == vertice]
 
 def grafo_inducido(grafo, subconjunto_vertices):
-    """
-    Devuelve el subgrafo inducido por un subconjunto de vértices.
-
+    '''
+    Dado un grafo en representacion de lista, y un subconjunto de vertices,
+    devuelva el subgrafo inducido
     Ejemplo Entrada:
-        grafo = (['A','B','C','D'], [('A','B'), ('A','C'), ('B','D')])
-        subconjunto_vertices = ['A','B','C']
-
-    Ejemplo Retorno:
-        (['A','B','C'], [('A','B'), ('A','C')])
-    """
-    pass
+    	grafo = ['4', 'A', 'B', 'C', 'D', 'A B', 'A C', 'B D']
+	subconjunto_vertices = ['A', 'B', 'C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'B'), ('A', 'C')])
+    '''
+    vertices, aristas = grafo
+    
+    aristas_inducidas = [arista for arista in aristas if arista[0] in subconjunto_vertices and arista[1] in subconjunto_vertices]
+    return (subconjunto_vertices, aristas_inducidas)
 
 def grafo_complementario(grafo):
-    """
-    Devuelve el grafo complementario (todos los pares de vértices que no son aristas).
-
+    '''
+    Dado un grafo en representacion de lista, devuelve el grafo complementario en forma de lista
     Ejemplo Entrada:
-        grafo = (['A','B','C'], [('A','B'), ('B','C')])
+    	['3', 'A', 'B', 'C', 'A B', 'B C']
+    Ejemplo formato salida:
+    	(['A', 'B', 'C'], [('A', 'C'), ('B', 'A'), ('C', 'A'), ('C', 'B')])
+    '''
+    vertices, aristas = grafo
+    aristas_set = set(aristas)
 
-    Ejemplo Retorno:
-        (['A','B','C'], [('A','C')])
-    """
-    pass
+    complemento_aristas = []
+
+    for i in range(len(vertices)):
+        for j in range(i + 1, len(vertices)):
+            v1 = vertices[i]
+            v2 = vertices[j]
+            if (v1, v2) not in aristas_set and (v2, v1) not in aristas_set:
+                complemento_aristas.append((v1, v2))
+
+    return (vertices, complemento_aristas)
